0856aea0f3e17ed7b405b8a385dae7bc805a71b1
Juanjo <juanjo.ng.83@gmail.com>
2012-12-31 16:23:48 +0100

    Prepare: Separate pbs.* into a common part pbs.h and pbs_rail.* for rail and pbs_water.* for water.

---
+++ src/pbs_rail.h
@@ -0,0 +1,43 @@
+/* $Id: pbs_rail.h $ */
+
+/*
+ * This file is part of OpenTTD.
+ * OpenTTD is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 2.
+ * OpenTTD is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with OpenTTD. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file pbs_rail.h PBS support routines for rail */
+
+#ifndef PBS_RAIL_H
+#define PBS_RAIL_H
+
+#include "direction_type.h"
+#include "vehicle_type.h"
+#include "pbs.h"
+
+TrackBits GetReservedTrackbits(TileIndex t);
+
+void SetRailStationPlatformReservation(TileIndex start, DiagDirection dir, bool b);
+
+bool TryReserveRailTrack(TileIndex tile, Track t, bool trigger_stations = true);
+void UnreserveRailTrack(TileIndex tile, Track t);
+
+PBSTileInfo FollowTrainReservation(const Train *v, Vehicle **train_on_res = NULL);
+Train *GetTrainForReservation(TileIndex tile, Track track);
+
+bool IsSafeWaitingPosition(const Train *v, TileIndex tile, Trackdir trackdir, bool include_line_end, bool forbid_90deg = false);
+bool IsWaitingPositionFree(const Train *v, TileIndex tile, Trackdir trackdir, bool forbid_90deg = false);
+
+/**
+ * Check whether some of tracks is reserved on a tile.
+ * @param tile The tile.
+ * @param tracks The tracks to test.
+ * @return true if at least on of the tracks \a tracks is reserved on tile \a tile.
+ */
+static inline bool HasReservedTracks(TileIndex tile, TrackBits tracks)
+{
+	return (GetReservedTrackbits(tile) & tracks) != TRACK_BIT_NONE;
+}
+
+#endif /* PBS_RAIL_H */
---
+++ src/pbs_water.h
@@ -0,0 +1,31 @@
+/* $Id: pbs_water.h $ */
+
+/*
+ * This file is part of OpenTTD.
+ * OpenTTD is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 2.
+ * OpenTTD is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with OpenTTD. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file pbs_water.h PBS water support routines. */
+
+#ifndef PBS_WATER_H
+#define PBS_WATER_H
+
+#include "direction_type.h"
+#include "vehicle_type.h"
+#include "pbs.h"
+
+/* Routines related to reservation of water tracks on a tile. */
+TrackBits GetReservedWaterTracks(TileIndex t);
+TrackBits GetFreeWaterTrackReservation(TileIndex t, TrackdirBits trackdirs);
+void SetWaterTrackReservation(TileIndex t, TrackBits tracks);
+
+bool WaterTrackMayExist(TileIndex t);
+bool SetWaterTrackReservation(TileIndex t, Track track, bool value);
+bool HasWaterTrackReservation(TileIndex t);
+bool HasWaterTracksReserved(TileIndex t, TrackBits tracks);
+bool DoWaterPathReservation(TileIndex t, Trackdir trackdir);
+bool IsWaterPositionFree(TileIndex tile, Trackdir trackdir);
+
+#endif /* PBS_WATER_H */
