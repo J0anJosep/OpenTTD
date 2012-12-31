cf5b0d1c7df9026845b0be83bd7db493b9743594
Juanjo <juanjo.ng.83@gmail.com>
2012-12-31 16:35:26 +0100

    Houses callback is called using the new cached catchment.

--- src/station_cmd.cpp
+++ src/station_cmd.cpp
@@ -3186,13 +3186,13 @@ void TriggerWatchedCargoCallbacks(Station *st)
 	if (cargoes == 0) return;
 
 	/* Loop over all houses in the catchment. */
-	Rect r = st->GetCatchmentRect();
-	TileArea ta(TileXY(r.left, r.top), TileXY(r.right, r.bottom));
-	TILE_AREA_LOOP(tile, ta) {
-		if (IsTileType(tile, MP_HOUSE)) {
-			WatchedCargoCallback(tile, cargoes);
-		}
-	}
+	const TileArea &ta = st->GetStationCatchmentArea();
+	const bool *footprint = st->GetStationCatchmentFootprint();
+	MASKED_TILE_AREA_LOOP(tile, ta, footprint) {
+ 		if (IsTileType(tile, MP_HOUSE)) {
+ 			WatchedCargoCallback(tile, cargoes);
+ 		}
+ 	}
 }
 
 /**
