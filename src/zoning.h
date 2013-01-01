1928a8e58c3c13059b9f797f995993aea59f63e2
Juanjo <juanjo.ng.83@gmail.com>
2013-01-01 12:08:43 +0100

    Add the base files to deal with stations catchment area.

---
+++ src/zoning.h
@@ -0,0 +1,34 @@
+/* $Id: zoning.h $ */
+
+/*
+ * This file is part of OpenTTD.
+ * OpenTTD is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 2.
+ * OpenTTD is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with OpenTTD. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file zoning.h Functions related with the catchment area layer. */
+
+#ifndef ZONING_H
+#define ZONING_H
+
+#include "station_type.h"
+#include "tilearea_type.h"
+#include "filters/filter_enums.h"
+#include "filters/filter_active.h"
+
+extern FilterActive _ca_controller;
+extern bool *_ca_layer;
+extern bool _stations_modified;
+
+void ResetCatchmentAreaLayer();
+void UpdateCatchmentAreaLayer();
+
+void UpdateCALayer(const TileArea check_area);
+void UpdateCALayer(ListType list_type, const uint id);
+void UpdateCALayer(StationID station);
+
+void RotateStationOnCALayer(StationID station);
+void CheckSingleStationCA(StationID station);
+
+#endif /* ZONING_H */
