395cad39da39a05dedd29f9dde44473859989993
Juanjo <juanjo.ng.83@gmail.com>
2013-01-01 12:48:42 +0100

    Lift the reserved tracks of water paths on a dock tile before changing its available tracks.

--- src/station_cmd.cpp
+++ src/station_cmd.cpp
@@ -43,6 +43,7 @@
 #include "waypoint_base.h"
 #include "waypoint_func.h"
 #include "pbs_rail.h"
+#include "pbs_water.h"
 #include "debug.h"
 #include "core/random_func.hpp"
 #include "company_base.h"
@@ -2559,6 +2560,7 @@ CommandCost CmdRotateDockTracks(TileIndex tile, DoCommandFlag flags, uint32 p1, 
 	if (ret.Failed()) return ret;
 
 	if (flags & DC_EXEC) {
+		if (HasWaterTrackReservation(tile)) LiftReservations(tile);
 		RotateDockTracks(tile);
 		MarkTileDirtyByTile(tile);
 	}
