46467b7b382a1225be52924b655e777864ae1fdc
Juanjo <juanjo.ng.83@gmail.com>
2013-01-01 12:14:16 +0100

    Add a widget to open a filter for stations on station directory. Also, filter the directory list accordingly.

--- src/script/api/script_window.hpp
+++ src/script/api/script_window.hpp
@@ -2335,6 +2335,7 @@ public:
 	enum StationListWidgets {
 		/* Name starts with ST instead of S, because of collision with SaveLoadWidgets */
 		WID_STL_CAPTION                              = ::WID_STL_CAPTION,                              ///< Caption of the window.
+		WID_STL_FILTER                               = ::WID_STL_FILTER,                               ///< Filter icon button.
 		WID_STL_LIST                                 = ::WID_STL_LIST,                                 ///< The main panel, list of stations.
 		WID_STL_SCROLLBAR                            = ::WID_STL_SCROLLBAR,                            ///< Scrollbar next to the main panel.
 
