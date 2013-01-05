2e58eed3da15af4cd7f4d3ee14ad9890696a6b03
Juanjo <juanjo.ng.83@gmail.com>
2013-01-05 19:52:36 +0100

    Add IsLockTile and use it when possible.
    Conflicts:
    
    	src/pbs_water.cpp

--- src/newgrf_canal.cpp
+++ src/newgrf_canal.cpp
@@ -62,7 +62,7 @@ struct CanalResolverObject : public ResolverObject {
 		case 0x80: {
 			int z = GetTileZ(this->tile);
 			/* Return consistent height within locks */
-			if (IsTileType(this->tile, MP_WATER) && IsLock(this->tile) && GetLockPart(this->tile) == LOCK_PART_UPPER) z--;
+			if (IsLockTile(this->tile) && GetLockPart(this->tile) == LOCK_PART_UPPER) z--;
 			return z;
 		}
 
