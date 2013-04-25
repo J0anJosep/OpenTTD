5f34c8f1bae720b2d114b085c3612ac574c91eed
Juanjo <juanjo.ng.83@gmail.com>
2013-04-25 14:01:19 +0200

    New window for showing details of a group.

--- src/window.cpp
+++ src/window.cpp
@@ -1204,6 +1204,7 @@ void ChangeWindowOwner(Owner old_owner, Owner new_owner)
 			case WC_ROADVEH_LIST:
 			case WC_SHIPS_LIST:
 			case WC_AIRCRAFT_LIST:
+			case WC_GROUP_DETAILS:
 			case WC_BUY_COMPANY:
 			case WC_COMPANY:
 			case WC_COMPANY_INFRASTRUCTURE:
