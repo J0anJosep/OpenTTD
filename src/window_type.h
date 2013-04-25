1b711fc095e4311ae3d1f300101507c498f97c89
Juanjo <juanjo.ng.83@gmail.com>
2013-04-25 15:06:14 +0200

    Add the window types for filters.

--- src/window_type.h
+++ src/window_type.h
@@ -326,6 +326,35 @@ enum WindowClass {
 	 */
 	WC_AIRCRAFT_LIST,
 
+	/**
+	 * Group/vehicle filter; %Window numbers:
+	 *   - Window number same as parent ( | 1 << 20 if filtering groups of group window) = #FilterWindowWidgets
+	 */
+	WC_VEHICLE_GROUP_FILTER,
+
+	/**
+	 * Catchment area window; %Window numbers:
+	 *   - 0 = #FilterWindowWidgets
+	 */
+	WC_CATCHMENT_AREA_WINDOW,
+
+	/**
+	 * Industry filter; % Window numbers:
+	 *   - 0 = #FilterWindowWidgets
+	 */
+	WC_INDUSTRY_FILTER,
+
+	/**
+	 * Town filter; % Window numbers:
+	 *   - 0 = #FilterWindowWidgets
+	 */
+	WC_TOWN_FILTER,
+
+	/**
+	 * Stations filter; % Window numbers:
+	 *   - #CompanyID = #FilterWindowWidgets
+	 */
+	WC_STATION_FILTER,
 
 	/**
 	 * Town view; %Window numbers:
