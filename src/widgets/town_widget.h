e207bca522b64fc140db5e92ca7c291355bd8c9a
Juanjo <juanjo.ng.83@gmail.com>
2013-04-25 21:09:27 +0200

    Add a widget to open a filter for towns on town directory. Also, filter the directory list accordingly.
    Conflicts:
    	src/script/api/game/game_window.hpp.sq

--- src/widgets/town_widget.h
+++ src/widgets/town_widget.h
@@ -16,6 +16,10 @@
 enum TownDirectoryWidgets {
 	WID_TD_SORT_ORDER,       ///< Direction of sort dropdown.
 	WID_TD_SORT_CRITERIA,    ///< Criteria of sort dropdown.
+	WID_TD_FILTER,           ///< Open town filter.
+	WID_TD_SORT_NAME,        ///< Sort by town name.
+	WID_TD_SORT_POPULATION,  ///< Sort by town population.
+	WID_TD_SORT_RATING,      ///< Sort by town rating of local company.
 	WID_TD_LIST,             ///< List of towns.
 	WID_TD_SCROLLBAR,        ///< Scrollbar for the town list.
 	WID_TD_WORLD_POPULATION, ///< The world's population.
