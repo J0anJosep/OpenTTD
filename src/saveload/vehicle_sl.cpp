9a6fc4570f91511bf3ee4bc899f40ce69bf64529
Juanjo <juanjo.ng.83@gmail.com>
2013-08-18 14:33:24 +0000

    Change: Add a wait counter for all vehicle types.

--- src/saveload/vehicle_sl.cpp
+++ src/saveload/vehicle_sl.cpp
@@ -616,6 +616,7 @@ const SaveLoad *GetVehicleDescription(VehicleType vt)
 		     SLE_VAR(Vehicle, progress,              SLE_UINT8),
 
 		     SLE_VAR(Vehicle, vehstatus,             SLE_UINT8),
+		 SLE_CONDVAR(Vehicle, wait_counter,          SLE_UINT16,             SLV_MULTITILE_DEPOTS, SL_MAX_VERSION),
 		 SLE_CONDVAR(Vehicle, last_station_visited,  SLE_FILE_U8  | SLE_VAR_U16,   SL_MIN_VERSION,   SLV_5),
 		 SLE_CONDVAR(Vehicle, last_station_visited,  SLE_UINT16,                   SLV_5, SL_MAX_VERSION),
 		 SLE_CONDVAR(Vehicle, last_loading_station,  SLE_UINT16,                 SLV_182, SL_MAX_VERSION),
@@ -729,8 +730,7 @@ const SaveLoad *GetVehicleDescription(VehicleType vt)
 		 SLE_CONDVAR(Train, flags,               SLE_UINT16,                 SLV_100, SL_MAX_VERSION),
 		SLE_CONDNULL(2, SLV_2, SLV_60),
 
-		 SLE_CONDVAR(Train, wait_counter,        SLE_UINT16,                 SLV_136, SL_MAX_VERSION),
-
+		 SLE_CONDVAR(Train, wait_counter,        SLE_UINT16,                 SLV_136, SLV_MULTITILE_DEPOTS),
 		SLE_CONDNULL(2, SLV_2, SLV_20),
 		 SLE_CONDVAR(Train, gv_flags,            SLE_UINT16,                 SLV_139, SL_MAX_VERSION),
 		SLE_CONDNULL(11, SLV_2, SLV_144), // old reserved space
@@ -911,6 +911,8 @@ void Load_VEHS()
 			case VEH_INVALID: // Savegame shouldn't contain invalid vehicles
 			default: SlErrorCorrupt("Invalid vehicle type");
 		}
+
+		if (IsSavegameVersionBefore(SLV_MULTITILE_DEPOTS)) assert(v->type == VEH_TRAIN || v->wait_counter == 0);
 
 		SlObject(v, GetVehicleDescription(vtype));
 
