e5297ec5995b3fa4c5130a772eb7a89d8beab5b4
Juanjo <juanjo.ng.83@gmail.com>
2013-09-26 10:56:20 +0000

    Add scrollbars to tracklist window and set min sizing.

--- src/script/api/script_window.hpp
+++ src/script/api/script_window.hpp
@@ -1725,8 +1725,10 @@ public:
 	enum MusicTrackSelectionWidgets {
 		WID_MTS_CAPTION                              = ::WID_MTS_CAPTION,                              ///< Window caption.
 		WID_MTS_LIST_LEFT                            = ::WID_MTS_LIST_LEFT,                            ///< Left button.
+		WID_MTS_LEFT_SCROLLBAR                       = ::WID_MTS_LEFT_SCROLLBAR,                       ///< Scrollbar of left list.
 		WID_MTS_PLAYLIST                             = ::WID_MTS_PLAYLIST,                             ///< Playlist.
 		WID_MTS_LIST_RIGHT                           = ::WID_MTS_LIST_RIGHT,                           ///< Right button.
+		WID_MTS_RIGHT_SCROLLBAR                      = ::WID_MTS_RIGHT_SCROLLBAR,                      ///< Scrollbar of right list.
 		WID_MTS_MUSICSET                             = ::WID_MTS_MUSICSET,                             ///< Music set selection.
 		WID_MTS_ALL                                  = ::WID_MTS_ALL,                                  ///< All button.
 		WID_MTS_OLD                                  = ::WID_MTS_OLD,                                  ///< Old button.
