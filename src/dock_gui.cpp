d41aae18d89e7102d4a821e850d96406be3b77ff
Juanjo <juanjo.ng.83@gmail.com>
2013-10-14 17:49:14 +0000

    Reset queued command when closing windows that can queue them.

--- src/dock_gui.cpp
+++ src/dock_gui.cpp
@@ -106,6 +106,7 @@ struct BuildDocksToolbarWindow : Window {
 
 	~BuildDocksToolbarWindow()
 	{
+		if (_thd.GetCallbackWnd() == this) this->OnPlaceObjectAbort();
 		if (_settings_client.gui.link_terraform_toolbar) DeleteWindowById(WC_SCEN_LAND_GEN, 0, false);
 	}
 
@@ -288,6 +289,7 @@ struct BuildDocksToolbarWindow : Window {
 		DeleteWindowById(WC_BUILD_DEPOT, TRANSPORT_WATER);
 		DeleteWindowById(WC_SELECT_STATION, 0);
 		DeleteWindowByClass(WC_BUILD_BRIDGE);
+		EraseQueuedTouchCommand();
 	}
 
 	virtual void OnPlacePresize(Point pt, TileIndex tile_from)
@@ -578,6 +580,7 @@ public:
 				this->LowerWidget(_ship_depot_direction + WID_BDD_X);
 				if (_settings_client.sound.click_beep) SndPlayFx(SND_15_BEEP);
 				UpdateDocksDirection();
+				EraseQueuedTouchCommand();
 				this->SetDirty();
 				break;
 		}
