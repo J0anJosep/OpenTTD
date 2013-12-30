e2db49c24e0f249f4c8d3113db1cdcc9a91ac65e
Juanjo <juanjo.ng.83@gmail.com>
2013-12-30 14:05:55 +0100

    Change: Update town directory when its filter is opened and town growth is updated.

--- src/town_cmd.cpp
+++ src/town_cmd.cpp
@@ -2621,6 +2621,17 @@ const CargoSpec *FindFirstCargoWithTownEffect(TownEffect effect)
 	return NULL;
 }
 
+static void UpdateTownDirectoryFilter()
+{
+	Window *w;
+	FOR_ALL_WINDOWS_FROM_BACK(w) {
+		if (w->window_class == WC_TOWN_FILTER) {
+			InvalidateWindowData(WC_TOWN_FILTER, 0, 2);
+			return;
+		}
+	}
+}
+
 /**
  * Change the cargo goal of a town.
  * @param tile Unused.
@@ -2650,6 +2661,7 @@ CommandCost CmdTownCargoGoal(TileIndex tile, DoCommandFlag flags, uint32 p1, uin
 	if (flags & DC_EXEC) {
 		t->goal[te] = p2;
 		UpdateTownGrowth(t);
+		UpdateTownDirectoryFilter();
 		InvalidateWindowData(WC_TOWN_VIEW, index);
 	}
 
@@ -2714,6 +2726,7 @@ CommandCost CmdTownGrowthRate(TileIndex tile, DoCommandFlag flags, uint32 p1, ui
 			SetBit(t->flags, TOWN_CUSTOM_GROWTH);
 		}
 		UpdateTownGrowth(t);
+		UpdateTownDirectoryFilter();
 		InvalidateWindowData(WC_TOWN_VIEW, p1);
 	}
 
@@ -3013,6 +3026,7 @@ static CommandCost TownActionFundBuildings(Town *t, DoCommandFlag flags)
 
 		/* Enable growth (also checking GameScript's opinion) */
 		UpdateTownGrowth(t);
+		UpdateTownDirectoryFilter();
 
 		/* Build a new house, but add a small delay to make sure
 		 * that spamming funding doesn't let town grow any faster
@@ -3568,6 +3582,7 @@ void TownsMonthlyLoop()
 	}
 
 	UpdateTownCargoBitmap();
+	UpdateTownDirectoryFilter();
 }
 
 void TownsYearlyLoop()
