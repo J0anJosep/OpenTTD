b72694c41538186d03f0527725fce1abdcd2ce6e
Juanjo <juanjo.ng.83@gmail.com>
2014-02-09 23:35:11 +0100

    Remove some code about old FSM airports.

--- src/vehicle_cmd.cpp
+++ src/vehicle_cmd.cpp
@@ -573,7 +573,8 @@ CommandCost CmdStartStopVehicle(TileIndex tile, DoCommandFlag flags, uint32 p1, 
 		case VEH_AIRCRAFT: {
 			Aircraft *a = Aircraft::From(v);
 			/* cannot stop airplane when in flight, or when taking off / landing */
-			if (!(v->vehstatus & VS_CRASHED) && a->state >= STARTTAKEOFF && a->state < TERM7) return_cmd_error(STR_ERROR_AIRCRAFT_IS_IN_FLIGHT);
+			// revise
+			//if (!(v->vehstatus & VS_CRASHED) && a->state >= STARTTAKEOFF && a->state < TERM7) return_cmd_error(STR_ERROR_AIRCRAFT_IS_IN_FLIGHT);
 			break;
 		}
 
