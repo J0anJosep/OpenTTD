3861c8105d97bee6b0b4b54ef2ae0244b9e1080d
Juanjo <juanjo.ng.83@gmail.com>
2014-03-15 01:47:06 +0100

    Add a min sizing for width of command errors and query windows.

--- src/misc_gui.cpp
+++ src/misc_gui.cpp
@@ -1114,6 +1114,7 @@ struct QueryWindow : public Window {
 	{
 		if (widget != WID_Q_TEXT) return;
 
+		size->width = GetMinSizing(NWST_MESSAGE_LENGTH, size->width);
 		Dimension d = GetStringMultiLineBoundingBox(this->message, *size);
 		d.width += WD_FRAMETEXT_LEFT + WD_FRAMETEXT_RIGHT;
 		d.height += WD_FRAMERECT_TOP + WD_FRAMERECT_BOTTOM;
