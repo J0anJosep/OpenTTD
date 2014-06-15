c38321a3d0f2bbddc1bcd0b1f9e2a4025af66c1b
Juanjo <juanjo.ng.83@gmail.com>
2014-06-15 16:46:21 +0200

    Keep settings window when automatically check sizings.

--- src/window.cpp
+++ src/window.cpp
@@ -1868,6 +1868,7 @@ void CheckWindowMinSizings(bool close)
 
 	if (_z_front_window == NULL || !close) return;
 
+	bool size_window = FindWindowById(WC_GAME_OPTIONS, WN_GAME_OPTIONS_GAME_OPTIONS) != NULL;
 	DeleteAllNonVitalWindows();
 	ReInitAllWindows();
 
@@ -1889,6 +1890,7 @@ void CheckWindowMinSizings(bool close)
 		}
 	}
 
+	if (size_window) ShowGameOptions();
 }
 
 /**
