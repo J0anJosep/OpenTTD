bf5465e0f29fd0d99fd172bddc2836f8d4b1ef40
Juanjo <juanjo.ng.83@gmail.com>
2014-06-16 23:36:39 +0200

    Massive rebuilding of groups of a company.
    Grouping can be done by orderlist, same visited stations, cargo, first engine or class type.

--- src/command_type.h
+++ src/command_type.h
@@ -320,6 +320,7 @@ enum Commands {
 	CMD_ADD_SHARED_VEHICLE_GROUP,     ///< add all other shared vehicles to a group which are missing
 	CMD_REMOVE_ALL_VEHICLES_GROUP,    ///< remove all vehicles from a group
 	CMD_SET_GROUP_REPLACE_PROTECTION, ///< set the autoreplace-protection for a group
+	CMD_BUILD_GROUPS_OF_VEHICLE_TYPE, ///< rebuild groups for a certain vehicle type
 
 	CMD_MOVE_ORDER,                   ///< move an order
 	CMD_CHANGE_TIMETABLE,             ///< change the timetable for a vehicle
