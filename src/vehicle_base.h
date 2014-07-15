bfe395fe305afeacf27dc9872c61f4fe4bb9cff1
Juanjo <juanjo.ng.83@gmail.com>
2014-07-15 21:28:07 +0200

    Add base functions for complex servicing.

--- src/vehicle_base.h
+++ src/vehicle_base.h
@@ -761,6 +761,11 @@ public:
 
 	bool HandleBreakdown();
 
+	bool IsServicing() const { return HasBit(this->vehicle_flags, VF_IS_SERVICING); }
+
+	void StartService();
+	bool ContinueServicing();
+
 	bool NeedsAutorenewing(const Company *c, bool use_renew_setting = true) const;
 
 	bool NeedsServicing() const;
