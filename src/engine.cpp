8a8b481825c58cc8231cbdb10651061468fa4a08
Juanjo <juanjo.ng.83@gmail.com>
2014-09-07 23:22:55 +0200

    Allow hiding not available vehicles.

--- src/engine.cpp
+++ src/engine.cpp
@@ -70,6 +70,7 @@ const uint EngineOverrideManager::NUM_DEFAULT_ENGINES = _engine_counts[VEH_TRAIN
 
 Engine::Engine() :
 	name(NULL),
+	company_hidden(0),
 	overrides_count(0),
 	overrides(NULL)
 {
@@ -81,6 +82,7 @@ Engine::Engine(VehicleType type, EngineID base)
 	this->grf_prop.local_id = base;
 	this->list_position = base;
 	this->preview_company = INVALID_COMPANY;
+	this->company_hidden = 0;
 
 	/* Check if this base engine is within the original engine data range */
 	if (base >= _engine_counts[type]) {
@@ -647,7 +649,6 @@ void StartupOneEngine(Engine *e, Date aging_date)
 	e->age = 0;
 	e->flags = 0;
 	e->company_avail = 0;
-	e->company_hidden = 0;
 
 	/* Don't randomise the start-date in the first two years after gamestart to ensure availability
 	 * of engines in early starting games.
@@ -861,7 +862,6 @@ CommandCost CmdSetVehicleVisibility(TileIndex tile, DoCommandFlag flags, uint32 
 {
 	Engine *e = Engine::GetIfValid(GB(p2, 0, 31));
 	if (e == NULL || _current_company >= MAX_COMPANIES) return CMD_ERROR;
-	if (!IsEngineBuildable(e->index, e->type, _current_company)) return CMD_ERROR;
 
 	if ((flags & DC_EXEC) != 0) {
 		SB(e->company_hidden, _current_company, 1, GB(p2, 31, 1));
