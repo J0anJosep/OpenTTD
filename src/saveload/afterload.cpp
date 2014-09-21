0c90749fed2450a5b70b6b8652e4dd53b88b2aa8
Juanjo <juanjo.ng.83@gmail.com>
2014-09-22 01:57:20 +0200

    Set the stuck state for ships.
    Basic conditions to stuck/unstuck a ship.
    Show the state on vehicle window.

--- src/saveload/afterload.cpp
+++ src/saveload/afterload.cpp
@@ -3077,6 +3077,13 @@ bool AfterLoadGame()
 		}
 	}
 
+	if (IsSavegameVersionBefore(SL_STUCK_SHIPS)) {
+		Ship *v;
+		FOR_ALL_SHIPS(v) {
+			v->stuck = false;
+		}
+	}
+
 	/* Road stops is 'only' updating some caches */
 	AfterLoadRoadStops();
 	AfterLoadLabelMaps();
