d65c97b299dff8612c2b1c35752b4b351d098e53
Juanjo <juanjo.ng.83@gmail.com>
2014-09-22 14:22:36 +0200

    Get better sizing of widgets.

--- src/network/network_content_gui.cpp
+++ src/network/network_content_gui.cpp
@@ -584,7 +584,7 @@ public:
 			}
 
 			case WID_NCL_MATRIX:
-				resize->height = max(this->checkbox_size.height, (uint)FONT_HEIGHT_NORMAL) + WD_MATRIX_TOP + WD_MATRIX_BOTTOM;
+				resize->height = GetMinSizing(NWST_STEP, max(this->checkbox_size.height, (uint)FONT_HEIGHT_NORMAL) + WD_MATRIX_TOP + WD_MATRIX_BOTTOM);
 				size->height = 10 * resize->height;
 				break;
 		}
