3643c113ab01acf4512bfef36d670dca622d8c28
Juanjo <juanjo.ng.83@gmail.com>
2014-09-22 12:56:44 +0200

    Store edges for water tiles.
    Also consider afterload and generating landscape.

--- src/landscape.cpp
+++ src/landscape.cpp
@@ -1291,6 +1291,7 @@ void GenerateLandscape(byte mode)
 	if (_settings_game.game_creation.landscape == LT_TROPIC) CreateDesertOrRainForest();
 
 	CreateRivers();
+	UpdateWaterTiles();
 }
 
 void OnTick_Town();
