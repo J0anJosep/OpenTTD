84374e86a3652d052f5d3d42c9dc65a787a5296c
Juanjo <juanjo.ng.83@gmail.com>
2014-09-22 12:56:44 +0200

    Store edges for water tiles.
    Also consider afterload and generating landscape.

--- src/saveload/afterload.cpp
+++ src/saveload/afterload.cpp
@@ -3065,6 +3065,11 @@ bool AfterLoadGame()
 		}
 	}
 
+	if (IsSavegameVersionBefore(SL_STORE_WATER_TRACKS)) {
+		/* Store the edges and tracks of water tiles. */
+		UpdateWaterTiles();
+	}
+
 	/* Road stops is 'only' updating some caches */
 	AfterLoadRoadStops();
 	AfterLoadLabelMaps();
