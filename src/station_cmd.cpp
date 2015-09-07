544d1ec2061eaba409060c1ca7fd8b187f7787a7
Juanjo <juanjo.ng.83@gmail.com>
2015-09-07 11:09:09 +0200

    Use airport tracks in the GetTileTracks proc.

--- src/station_cmd.cpp
+++ src/station_cmd.cpp
@@ -3157,6 +3157,10 @@ static TrackStatus GetTileTrackStatus_Station(TileIndex tile, TransportType mode
 			}
 			break;
 
+		case TRANSPORT_AIR:
+			if (IsAirportTile(tile) && MayHaveAirTracks(tile)) trackbits = GetAirportTileTracks(tile);
+			break;
+
 		default:
 			break;
 	}
