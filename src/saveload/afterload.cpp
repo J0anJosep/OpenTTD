8b4c98c2b2680c9cc588263426c1443ef8197ec1
Juanjo <juanjo.ng.83@gmail.com>
2016-01-17 20:26:50 +0100

    Feature: Add a new setting for rectangular/precise algorithms about catchment areas.

--- src/saveload/afterload.cpp
+++ src/saveload/afterload.cpp
@@ -754,6 +754,7 @@ bool AfterLoadGame()
 		}
 	}
 	if (IsSavegameVersionBefore(SLV_STUCK_SHIPS)) _settings_game.pf.ship_path_reservation = false;
+	if (IsSavegameVersionBefore(SLV_CATCHMENT_AREAS)) _settings_game.station.precise_catchment = false;
 
 	/* Load the sprites */
 	GfxLoadSprites();
