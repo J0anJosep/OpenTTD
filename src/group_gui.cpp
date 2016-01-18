8dc9a4f27db5110285fc3fbe03c1d0f2ea80054c
Juanjo <juanjo.ng.83@gmail.com>
2016-01-18 23:34:44 +0100

    Set group sorters for vehicle windows.

--- src/group_gui.cpp
+++ src/group_gui.cpp
@@ -36,8 +36,6 @@
 
 static const int LEVEL_WIDTH = 10; ///< Indenting width of a sub-group in pixels
 
-typedef GUIList<const Group*> GUIGroupList;
-
 static const NWidgetPart _nested_group_widgets[] = {
 	NWidget(NWID_HORIZONTAL), // Window header
 		NWidget(WWT_CLOSEBOX, COLOUR_GREY),
@@ -140,29 +138,6 @@ private:
 		}
 	}
 
-	/** Sort the groups by their name */
-	static int CDECL GroupNameSorter(const Group * const *a, const Group * const *b)
-	{
-		static const Group *last_group[2] = { NULL, NULL };
-		static char         last_name[2][64] = { "", "" };
-
-		if (*a != last_group[0]) {
-			last_group[0] = *a;
-			SetDParam(0, (*a)->index);
-			GetString(last_name[0], STR_GROUP_NAME, lastof(last_name[0]));
-		}
-
-		if (*b != last_group[1]) {
-			last_group[1] = *b;
-			SetDParam(0, (*b)->index);
-			GetString(last_name[1], STR_GROUP_NAME, lastof(last_name[1]));
-		}
-
-		int r = strnatcmp(last_name[0], last_name[1]); // Sort by name (natural sorting).
-		if (r == 0) return (*a)->index - (*b)->index;
-		return r;
-	}
-
 	/**
 	 * (Re)Build the group list.
 	 *
@@ -185,7 +160,6 @@ private:
 		}
 
 		list.ForceResort();
-		list.Sort(&GroupNameSorter);
 
 		AddParents(&list, INVALID_GROUP, 0);
 
@@ -366,6 +340,7 @@ public:
 		this->groups.ForceRebuild();
 		this->groups.NeedResort();
 		this->BuildGroupList(vli.company);
+		this->groups.Sort(group_sorter_funcs[this->groups.SortType()]);
 
 		this->GetWidget<NWidgetCore>(WID_GL_CAPTION)->widget_data = STR_VEHICLE_LIST_TRAIN_CAPTION + this->vli.vtype;
 		this->GetWidget<NWidgetCore>(WID_GL_LIST_VEHICLE)->tool_tip = STR_VEHICLE_LIST_TRAIN_LIST_TOOLTIP + this->vli.vtype;
@@ -507,6 +482,7 @@ public:
 		this->SortVehicleList();
 
 		this->BuildGroupList(this->owner);
+		this->groups.Sort(group_sorter_funcs[this->groups.SortType()]);
 
 		this->group_sb->SetCount(this->groups.Length());
 		this->vscroll->SetCount(this->vehicles.Length());
