144c3f3fa8ce145ec75ff1a00cc0a4ab38004940
Juanjo <juanjo.ng.83@gmail.com>
2016-01-27 18:41:16 +0100

    Filtering group items and group hierarchy are conflictive. Deal with it.
    *** Deal with group hierarchy in GUI.

--- src/settings_type.h
+++ src/settings_type.h
@@ -157,6 +157,7 @@ struct GUISettings {
 	uint8  settings_restriction_mode;        ///< selected restriction mode in adv. settings GUI. @see RestrictionMode
 	bool   newgrf_show_old_versions;         ///< whether to show old versions in the NewGRF list
 	uint8  newgrf_default_palette;           ///< default palette to use for NewGRFs without action 14 palette information
+	bool   show_compact_groups_when_filtering;     ///< whether to show a compact list of groups or the complete list when filtering groups
 
 	/**
 	 * Returns true when the user has sufficient privileges to edit newgrfs on a running game
