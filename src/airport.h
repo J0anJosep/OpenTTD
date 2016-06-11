accc7ce415d6e0e9fb76292aeea7629343de9213
Juanjo! <juanjo.ng.83@gmail.com>
2016-06-12 00:47:43 +0200

    Add new airport flags.

--- src/airport.h
+++ src/airport.h
@@ -22,6 +22,8 @@ static const uint NUM_AIRPORTTILES       = 256;              ///< Total number o
 static const uint NEW_AIRPORTTILE_OFFSET = 74;               ///< offset of first newgrf airport tile
 static const uint INVALID_AIRPORTTILE    = NUM_AIRPORTTILES; ///< id for an invalid airport tile
 
+static const uint LONG_RUNWAY_LENGTH     = 6;                ///< Runways with 5 or less tiles are considered short.
+
 /** Airport types */
 enum AirportTypes {
 	AT_SMALL           =   0, ///< Small airport.
@@ -39,6 +41,30 @@ enum AirportTypes {
 	NUM_AIRPORTS       = 128, ///< Maximal number of airports in total.
 	AT_INVALID         = 254, ///< Invalid airport.
 	AT_DUMMY           = 255, ///< Dummy airport.
+};
+
+enum AirportFlags {
+	AF_NONE                =  0,       ///< No flag.
+	AF_CLOSED_MANUAL       =  1 <<  0, ///< The airport is closed manually.
+	AF_CLOSED_ZEP_CRASH    =  1 <<  1, ///< The airport is closed due to a zeppelin crash.
+	AF_CLOSED_REG_CRASH    =  1 <<  2, ///< The airport is closed due to a regular aircraft crash.
+	AF_CLOSED_DESIGN       =  1 <<  3, ///< The airport is closed due to a design modification.
+	AF_CLOSED              =  AF_CLOSED_MANUAL | AF_CLOSED_ZEP_CRASH | AF_CLOSED_REG_CRASH | AF_CLOSED_DESIGN,
+
+	AF_HELIPADS            =  1 <<  8, ///< Airport has helipads.
+	AF_HELIPORTS           =  1 <<  9, ///< Airport has heliports.
+	AF_HELICOPTERS         =  AF_HELIPADS | AF_HELIPORTS,
+
+	/* Short: 5 or less tiles. */
+	AF_SHORT_TAKEOFF       =  1 << 10, ///< Airport has a short takeoff runway.
+	AF_LONG_TAKEOFF        =  1 << 11, ///< Airport has a long takeoff runway.
+	AF_TAKEOFF             =  AF_SHORT_TAKEOFF | AF_LONG_TAKEOFF,
+
+	AF_SHORT_LANDING       =  1 << 12, ///< Airport has a short landing runway.
+	AF_LONG_LANDING        =  1 << 13, ///< Airport has a long landing runway.
+	AF_LANDING             =  AF_SHORT_LANDING | AF_LONG_LANDING,
+
+	AF_STRUCTURE_MASK      =  AF_HELICOPTERS | AF_TAKEOFF | AF_LANDING,
 };
 
 #endif /* AIRPORT_H */
