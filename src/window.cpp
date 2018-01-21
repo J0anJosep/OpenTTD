cf0d37834cb8f17c302da597c107b6392592f3c5
J0anJosep <juanjo.ng.83@gmail.com>
2018-01-21 10:33:51 +0100

    Add a shortcut for deleting news windows and any pending news windows as well.

--- src/window.cpp
+++ src/window.cpp
@@ -36,6 +36,7 @@
 #include "error.h"
 #include "game/game.hpp"
 #include "video/video_driver.hpp"
+#include "news_func.h"
 
 #include "safeguards.h"
 
@@ -3309,6 +3310,15 @@ restart_search:
 			goto restart_search;
 		}
 	}
+}
+
+/**
+ * Delete all news windows and all pending news windows as well. */
+void DeleteAllNewsWindows()
+{
+	InvalidateWindowData(WC_STATUS_BAR, 0, SBI_NEWS_DELETED); // invalidate the statusbar
+	DeleteWindowById(WC_NEWS_WINDOW, 0); // close the newspapers window if shown
+	InitNewsItemStructs();
 }
 
 /**
