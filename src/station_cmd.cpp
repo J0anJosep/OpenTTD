ba370dbe653d21426ef700fe0a89aa8c2ba907e3
J0anJosep <juanjo.ng.83@gmail.com>
2018-01-28 22:36:54 +0100

    future comments for helicopters

--- src/station_cmd.cpp
+++ src/station_cmd.cpp
@@ -3848,6 +3848,9 @@ void DeleteOilRig(TileIndex tile)
 
 	st->rect.AfterRemoveTile(st, tile);
 
+	// revise helicopters must take off
+	// helicopters heading here must update...
+
 	st->UpdateVirtCoord();
 	st->UpdateCatchment();
 	st->RecomputeIndustriesNear();
