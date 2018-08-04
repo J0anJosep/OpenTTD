pelya/openttd-android

Add scrollbars to tracklist window and set min sizing.

src/widgets/music_widget.h
@@ -14,16 +14,18 @@
 
 /** Widgets of the #MusicTrackSelectionWindow class. */
 enum MusicTrackSelectionWidgets {
-	WID_MTS_LIST_LEFT,  ///< Left button.
-	WID_MTS_PLAYLIST,   ///< Playlist.
-	WID_MTS_LIST_RIGHT, ///< Right button.
-	WID_MTS_ALL,        ///< All button.
-	WID_MTS_OLD,        ///< Old button.
-	WID_MTS_NEW,        ///< New button.
-	WID_MTS_EZY,        ///< Ezy button.
-	WID_MTS_CUSTOM1,    ///< Custom1 button.
-	WID_MTS_CUSTOM2,    ///< Custom2 button.
-	WID_MTS_CLEAR,      ///< Clear button.
+	WID_MTS_LIST_LEFT,       ///< Left list.
+	WID_MTS_LEFT_SCROLLBAR,  ///< Scrollbar of left list.
+	WID_MTS_PLAYLIST,        ///< Playlist name.
+	WID_MTS_LIST_RIGHT,      ///< Right list.
+	WID_MTS_RIGHT_SCROLLBAR, ///< Scrollbar of right list.
+	WID_MTS_ALL,             ///< All button.
+	WID_MTS_OLD,             ///< Old button.
+	WID_MTS_NEW,             ///< New button.
+	WID_MTS_EZY,             ///< Ezy button.
+	WID_MTS_CUSTOM1,         ///< Custom1 button.
+	WID_MTS_CUSTOM2,         ///< Custom2 button.
+	WID_MTS_CLEAR,           ///< Clear button.
 };
 
 /** Widgets of the #MusicWindow class. */

J0anJosep/OpenTTD

src/widgets/music_widget.h
@@ -14,18 +14,20 @@
 
 /** Widgets of the #MusicTrackSelectionWindow class. */
 enum MusicTrackSelectionWidgets {
-	WID_MTS_CAPTION,    ///< Window caption.
-	WID_MTS_LIST_LEFT,  ///< Left button.
-	WID_MTS_PLAYLIST,   ///< Playlist.
-	WID_MTS_LIST_RIGHT, ///< Right button.
-	WID_MTS_MUSICSET,   ///< Music set selection.
-	WID_MTS_ALL,        ///< All button.
-	WID_MTS_OLD,        ///< Old button.
-	WID_MTS_NEW,        ///< New button.
-	WID_MTS_EZY,        ///< Ezy button.
-	WID_MTS_CUSTOM1,    ///< Custom1 button.
-	WID_MTS_CUSTOM2,    ///< Custom2 button.
-	WID_MTS_CLEAR,      ///< Clear button.
+	WID_MTS_CAPTION,         ///< Window caption.
+	WID_MTS_LIST_LEFT,       ///< Left button.
+	WID_MTS_LEFT_SCROLLBAR,  ///< Scrollbar of left list.
+	WID_MTS_PLAYLIST,        ///< Playlist.
+	WID_MTS_LIST_RIGHT,      ///< Right button.
+	WID_MTS_RIGHT_SCROLLBAR, ///< Scrollbar of right list.
+	WID_MTS_MUSICSET,        ///< Music set selection.
+	WID_MTS_ALL,             ///< All button.
+	WID_MTS_OLD,             ///< Old button.
+	WID_MTS_NEW,             ///< New button.
+	WID_MTS_EZY,             ///< Ezy button.
+	WID_MTS_CUSTOM1,         ///< Custom1 button.
+	WID_MTS_CUSTOM2,         ///< Custom2 button.
+	WID_MTS_CLEAR,           ///< Clear button.
 };
 
 /** Widgets of the #MusicWindow class. */