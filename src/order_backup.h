449d240fc6d6a6f6ba3facdf21dfffc082eafeb6
J0anJosep <juanjo.ng.83@gmail.com>
2021-01-09 17:19:03 +0100

    Change: Go to hangar orders store the DepotID instead of the StationID.

--- src/order_backup.h
+++ src/order_backup.h
@@ -61,7 +61,7 @@ public:
 
 	static void ClearGroup(GroupID group);
 	static void ClearVehicle(const Vehicle *v);
-	static void RemoveOrder(OrderType type, DestinationID destination, bool hangar);
+	static void RemoveOrder(OrderType type, DestinationID destination);
 };
 
 #endif /* ORDER_BACKUP_H */
