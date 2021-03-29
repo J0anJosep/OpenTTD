34ff75c8c5b9524f30fdb82ed93f9688e048ee83
J0anJosep <juanjo.ng.83@gmail.com>
2021-03-29 21:03:40 +0200

    Add: Set reservation of extended ship depots and prepare for setting it for road and train depots.

--- src/depot.cpp
+++ src/depot.cpp
@@ -158,3 +158,35 @@ void Depot::RescanDepotTiles()
 		InvalidateWindowData(WC_BUILD_VEHICLE, this->index, 0, true);
 	}
 }
+
+/**
+ * Fix tile reservations on big depots and vehicle changes.
+ * @param v Vehicle to be revised.
+ * @param reserve Whether to reserve or free the position v is occupying.
+ */
+void UpdateExtendedDepotReservation(Vehicle *v, bool reserve)
+{
+	assert(v != nullptr);
+	assert(IsExtendedDepotTile(v->tile));
+	DepotReservation res_type = DEPOT_RESERVATION_EMPTY;
+
+	res_type = (v->vehstatus & VS_STOPPED) ?
+			DEPOT_RESERVATION_FULL_STOPPED_VEH : DEPOT_RESERVATION_IN_USE;
+
+	switch (v->type) {
+		case VEH_ROAD:
+			break;
+
+		case VEH_SHIP:
+			SetDepotReservation(v->tile, res_type);
+			break;
+
+		case VEH_TRAIN:
+			break;
+
+		case VEH_AIRCRAFT:
+			break;
+
+		default: NOT_REACHED();
+	}
+}
