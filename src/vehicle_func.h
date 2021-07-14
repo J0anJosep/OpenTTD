f163898a2c6e60a502c354b1f85854ac5645dbc1
J0anJosep <juanjo.ng.83@gmail.com>
2021-07-14 14:08:36 +0200

    Add: Include a function to check if an airport has a free hangar given one of its tiles.

--- src/vehicle_func.h
+++ src/vehicle_func.h
@@ -165,6 +165,7 @@ static inline StringID GetCmdSendToDepotMsg(const BaseVehicle *v)
 CommandCost EnsureNoVehicleOnGround(TileIndex tile);
 CommandCost EnsureNoVisibleVehicleOnGround(TileIndex tile);
 CommandCost EnsureNoTrainOnTrackBits(TileIndex tile, TrackBits track_bits);
+CommandCost EnsureFreeHangar(TileIndex tile);
 
 bool CanVehicleUseStation(EngineID engine_type, const struct Station *st);
 bool CanVehicleUseStation(const Vehicle *v, const struct Station *st);
