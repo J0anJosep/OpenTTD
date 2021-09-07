04f432dc061af3e956c7182b769a7309d29009a5
J0anJosep <juanjo.ng.83@gmail.com>
2021-09-07 20:47:09 +0200

    Change: Amend zeppelin disaster to new airports (zeppelin disaster has less probability of happening).

--- src/saveload/afterload.cpp
+++ src/saveload/afterload.cpp
@@ -3417,6 +3417,9 @@ bool AfterLoadGame()
 	}
 
 	if (IsSavegameVersionBefore(SLV_MULTITILE_AIRPORTS)) {
+		/* Delete already crashed zeppelins. */
+		DeleteCrashedZeppelins();
+
 		/* We have to destroy all aircraft to completely restart from scratch. */
 		for (Aircraft *v : Aircraft::Iterate()) {
 			if ((v->vehstatus & VS_CRASHED) == 0) v->Crash();
