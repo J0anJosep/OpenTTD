f5a4af4e8f66fcf318937ff9e7959ecf62a350c3
J0anJosep <juanjo.ng.83@gmail.com>
2021-10-02 17:39:31 +0200

    Add: Set reservation and penalties to improve pathfinding in extended depots.

--- src/train_cmd.cpp
+++ src/train_cmd.cpp
@@ -2264,6 +2264,7 @@ bool HandleTrainEnterDepot(Train *v)
 		for (Train *u = t; u != nullptr; u = u->Next()) u->track |= TRACK_BIT_DEPOT;
 		t->force_proceed = TFP_NONE;
 		ClrBit(t->flags, VRF_TOGGLE_REVERSE);
+		UpdateExtendedDepotReservation(t, true);
 		v->UpdateViewport(true, true);
 		SetWindowClassesDirty(WC_TRAINS_LIST);
 		SetWindowDirty(WC_VEHICLE_VIEW, v->index);
@@ -2361,6 +2362,7 @@ static bool CheckTrainStayInDepot(Train *v)
 		v->cur_speed = 0;
 		v->UpdateAcceleration();
 		if (CheckReverseTrain(v)) ReverseTrainDirection(v);
+		UpdateExtendedDepotReservation(v, false);
 		InvalidateWindowData(WC_VEHICLE_DEPOT, depot_id);
 
 		/* Check whether it is safe to exit the depot. */
@@ -3173,6 +3175,7 @@ uint Train::Crash(bool flooded)
 				for (Train *v = this; v != nullptr; v = v->Next()) {
 					v->track &= ~TRACK_BIT_DEPOT;
 				}
+				UpdateExtendedDepotReservation(this, false);
 				InvalidateWindowData(WC_VEHICLE_DEPOT, GetDepotIndex(this->tile));
 			}
 			/* Remove reserved tracks of platform ahead. */
